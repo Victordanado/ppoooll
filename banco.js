e56776feed1f86133c3fc6a931fca1549c7192d1
Victordanado <hugo.silva26@escola.pr.gov.br>
2026-03-16 10:28:45 -0300

    xsss

--- banco.js
+++ banco.js
@@ -1,1 +1,21 @@
+let saldo = document.getElementById("saldo")
 
+let visivel = true
+
+document.querySelector(".verSaldo").onclick = function(){
+
+if(visivel){
+
+saldo.innerText = "••••••"
+
+visivel = false
+
+}else{
+
+saldo.innerText = "R$ 2.500,00"
+
+visivel = true
+
+}
+
+}
